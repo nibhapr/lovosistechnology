8271cba6ac64ff3d939b9ed350b1f2967b94effc
nibhapr <nibhapr@gmail.com>
2024-12-21 15:58:04 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,160 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
+  site: "https://lovosis.com/",
+  /*image: {
+    domains: ["images.unsplash.com"],
+  },
+  pages: ["./src/pages", "./src/pages/products/[id].astro"],
+  components: [
+    "./src/components",
+   "./src/components/sections/products/[id].astro",
+  ],*/
+
+  prefetch: true,
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en",
+          // The `defaultLocale` value must present in `locales` keys
+          fr: "fr",
+        },
+      },
+    }),
+    starlight({
+      title: "Lovosis Doc",
+      defaultLocale: "root",
+      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
+      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
+      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
+      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
+      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
+        es: {
+          label: "Español",
+          lang: "es",
+        },
+        fa: {
+          label: "Persian",
+          lang: "fa",
+          dir: "rtl",
+        },
+        fr: {
+          label: "Français",
+          lang: "fr",
+        },
+        ja: {
+          label: "日本語",
+          lang: "ja",
+        },
+        "zh-cn": {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      // https://starlight.astro.build/guides/sidebar/
+      sidebar: [
+        {
+          label: "Quick Start Guides",
+          translations: {
+            de: "Schnellstartanleitungen",
+            es: "Guías de Inicio Rápido",
+            fa: "راهنمای شروع سریع",
+            fr: "Guides de Démarrage Rapide",
+            ja: "クイックスタートガイド",
+            "zh-cn": "快速入门指南",
+          },
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Tools & Equipment",
+          items: [
+            {
+              label: "Tool Guides",
+              link: "tools/tool-guides/",
+            },
+            {
+              label: "Equipment Care",
+              link: "tools/equipment-care/",
+            },
+          ],
+        },
+        {
+          label: "Construction Services",
+          autogenerate: {
+            directory: "construction",
+          },
+        },
+        {
+          label: "Advanced Topics",
+          autogenerate: {
+            directory: "advanced",
+          },
+        },
+      ],
+      social: {
+        github: "https://github.com/mearashadowfax/ScrewFast",
+      },
+      disable404Route: true,
+      customCss: ["./src/assets/styles/starlight.css"],
+      favicon: "/favicon.ico",
+      components: {
+        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
+        Head: "./src/components/ui/starlight/Head.astro",
+        MobileMenuFooter:
+          "./src/components/ui/starlight/MobileMenuFooter.astro",
+        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
+      },
+      head: [
+        {
+          tag: "meta",
+          attrs: {
+            property: "og:image",
+            content: "https://lovosis.com/" + "/social.webp",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "twitter:image",
+            content: "https://lovosis.com/" + "/social.webp",
+          },
+        },
+      ],
+    }),
+    compressor({
+      gzip: false,
+      brotli: true,
+    }),
+    react(),
+  ],
+  output: "static",
+  experimental: {
+    clientPrerender: true,
+    directRenderScript: true,
+  },
+  adapter: vercelStatic(),
+});
