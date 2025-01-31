5f69e951ec78194171f39541b145934b6fa6901e
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-31 14:56:09 +0530

    unv structured schema

--- src/data/camera.js
+++ src/data/camera.js
@@ -1,60 +1,92 @@
-
-
 export const cameraData = [
-    {
-      title: "Uho-P2A-M3F4D",
-      description: "High quality image with 3MP, 1/3,2304*1296@20fps in the main stream,Built-in Mic & Speaker, support two-way audio, offer better interaction,Support sound and light warning, makes warning and alarming more noticeable",
-      image: "/pictures/Uho-P2A-M3F4D.png",
-    },
-    {
-      title: "Uho-P3C-M5F4",
-      description: "High quality image with 5MP, 1/1.8,5MP (2880*1620)@25/20fps; 4MP (2560*1440)@25/20fps; 3MP (2304*1296)@30/25fps; 2MP (1920*1080)@30/25fps;H.264,-Colorhunter%20with%20Wise,Colorhunter with Wise-ISP technology ensures 24-hour full-color images,Support digital WDR (Wide Dynamic Range)",
-      image: "/pictures/Uho-P3C-M5F4.png",
-    },
-    {
-      title: "Uho-S3S-M33D",
-      description: "Fixed camera and PT camera integration, dual channels with one IP, look at overall situation and consider details at the same time,Ultra 265, H.265, H.264,Support digital WDR (Wide Dynamic Range),Built-in Mic & Speaker, support two-way audio, offer better interaction",
-      image: "/pictures/Uho-S3S-M33D.png",
-    },
-    {
-      title: "Uho-B2D-M5F3D",
-      description: "High quality image with 5MP, 1/3,Ultra 265, H.265, H.264, MJPEG,Smart IR, up to 20m (66ft) IR distance,Support 128 G Micro SD card",
-      image: "/pictures/Uho-B2D-M5F3D.jpg",
-    },
-  ];
-  export const LovosisData = [
-    {
-      title: "Authorized UNV Distributor",
-      description: "We are the official distributor of UNV cameras and UNV CCTV systems in Dubai, providing you with direct access to the latest products and innovations",
-      image: "/pictures/icons/icon2.png",
-    },
-    {
-      title: "Wide Range of UNV CCTV Products",
-      description: " Our product range includes everything from Uniview cameras to complete CCTV systems, ensuring that you find the perfect solution for your security needs.",
-      image: "/pictures/icons/icon.png",
-    },
-    {
-      title: "Competitive UNV CCTV Camera Prices",
-      description: "With our established network and bulk purchasing power, we offer the best UNV CCTV camera prices in the Middle East, providing you with unbeatable value.",
-      image: "/pictures/icons/icon6.png",
-    },
-   
-  ];
-  export const unvData = [
-    {
-      title: "Superior Video Quality:",
-      description: "UNV cameras provide crystal-clear 4K resolution, ensuring every detail is captured.",
-      image: "/pictures/icons/icon45.png",
-    },
-    {
-      title: "Advanced AI Technology",
-      description: "UNV CCTV systems are equipped with AI-powered features like facial recognition, motion detection, and intrusion alerts",
-      image: "/pictures/icons/icon7.png",
-    },
-    {
-      title: "Competitive UNV CCTV Camera Prices",
-      description: "With our established network and bulk purchasing power, we offer the best UNV CCTV camera prices in the Middle East, providing you with unbeatable value.",
-      image: "/pictures/icons/icon6.png",
-    },
-   
-  ];
+  {
+    title: "Uho-P2A-M3F4D",
+    description:
+      "High quality image with 3MP, 1/3,2304*1296@20fps in the main stream,Built-in Mic & Speaker, support two-way audio, offer better interaction,Support sound and light warning, makes warning and alarming more noticeable",
+    image: "/pictures/Uho-P2A-M3F4D.png",
+  },
+  {
+    title: "Uho-P3C-M5F4",
+    description:
+      "High quality image with 5MP, 1/1.8,5MP (2880*1620)@25/20fps; 4MP (2560*1440)@25/20fps; 3MP (2304*1296)@30/25fps; 2MP (1920*1080)@30/25fps;H.264,-Colorhunter%20with%20Wise,Colorhunter with Wise-ISP technology ensures 24-hour full-color images,Support digital WDR (Wide Dynamic Range)",
+    image: "/pictures/Uho-P3C-M5F4.png",
+  },
+  {
+    title: "Uho-S3S-M33D",
+    description:
+      "Fixed camera and PT camera integration, dual channels with one IP, look at overall situation and consider details at the same time,Ultra 265, H.265, H.264,Support digital WDR (Wide Dynamic Range),Built-in Mic & Speaker, support two-way audio, offer better interaction",
+    image: "/pictures/Uho-S3S-M33D.png",
+  },
+  {
+    title: "Uho-B2D-M5F3D",
+    description:
+      "High quality image with 5MP, 1/3,Ultra 265, H.265, H.264, MJPEG,Smart IR, up to 20m (66ft) IR distance,Support 128 G Micro SD card",
+    image: "/pictures/Uho-B2D-M5F3D.jpg",
+  },
+];
+export const univiewData = [
+  {
+    title: "IPC6622SR-X25-VF",
+    description:
+      "High quality image with 2MP,1/2.8’’ CMOS sensor,1920*1080@60fps in the main stream,Ultra 265, H.265, H.264, MJPEG,33X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring, suitable for different statistical scenarios,Lighthunter technology ensures ultra-high image quality in low illumination environment",
+    image: "/pictures/unv/unv1.png",
+  },
+  {
+    title: "IPC6658SR-X25-VF",
+    description:
+      "High quality image with 8MP,1/1.8’’ CMOS sensor,3840*2160@30fps in the main stream,Ultra 265, H.265, H.264, MJPEG,25X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring, suitable for different statistical scenarios,Lighthunter technology ensures ultra-high image quality in low illumination environment",
+    image: "/pictures/unv/unv2.png",
+  },
+  {
+    title: "IPC6315LFW-AX5C-VG1",
+    description:
+      "High quality image with 5MP, 1/2.8, CMOS sensorUltra 265, H.265, H.264, MJPEG,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,LightHunter technology ensures ultra-high image quality in low illumination environment,Up to 120 dB WDR (Wide Dynamic Range),Built-in Mic & Speaker,Smart IR, Up to 50 m (164 ft) IR distance and 15m (49 ft) warm light distance,Micro SD, up to 512GB",
+    image: "/pictures/unv/unv3.png",
+  },
+  {
+    title: "IPC6424SR-X25-VF-B",
+    description:
+      "High quality image with 4MP,1/2.8’’ CMOS sensor,2688*1520@30fps in the main stream,Ultra 265, H.265, H.264, MJPEG,25X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring, suitable for different statistical scenarios,Lighthunter technology ensures ultra-high image quality in low illumination environment",
+    image: "/pictures/unv/unv4.png",
+  },
+];
+export const LovosisData = [
+  {
+    title: "Authorized UNV Distributor",
+    description:
+      "We are the official distributor of UNV cameras and UNV CCTV systems in Dubai, providing you with direct access to the latest products and innovations",
+    image: "/pictures/icons/icon2.png",
+  },
+  {
+    title: "Wide Range of UNV CCTV Products",
+    description:
+      " Our product range includes everything from Uniview cameras to complete CCTV systems, ensuring that you find the perfect solution for your security needs.",
+    image: "/pictures/icons/icon.png",
+  },
+  {
+    title: "After-Sales Service and Warranty",
+    description:
+      "Customer satisfaction is a top priority for Lovosis Technology. They offer comprehensive after-sales services, including maintenance, repairs, and software updates.",
+    image: "/pictures/icons/sales.png",
+  },
+];
+export const unvData = [
+  {
+    title: "Superior Video Quality:",
+    description:
+      "UNV cameras provide crystal-clear 4K resolution, ensuring every detail is captured.",
+    image: "/pictures/icons/icon45.png",
+  },
+  {
+    title: "Advanced AI Technology",
+    description:
+      "UNV CCTV systems are equipped with AI-powered features like facial recognition, motion detection, and intrusion alerts",
+    image: "/pictures/icons/icon7.png",
+  },
+  {
+    title: "Competitive UNV CCTV Camera Prices",
+    description:
+      "With our established network and bulk purchasing power, we offer the best UNV CCTV camera prices in the Middle East, providing you with unbeatable value.",
+    image: "/pictures/icons/icon6.png",
+  },
+];
